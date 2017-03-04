c8549bed3c58e30ebec7a9c8c379d12a1d9791bc
Eric Ma <ericmajinglong@gmail.com>
2017-03-04 18:43:06 -0500

    updated build script to continually update pdf template

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pandoc cover-letter.md -o cover-letter.docx -o cover-letter.pdf
 # Copy stuff into local directory as needed.
 cp ../references-master/papers-library.bib .
 cp ../styles/pnas.csl .
+cp ../pandoc-templates/default.latex .
 
 # Make figures
 python convert_images.py
@@ -13,6 +14,7 @@ pandoc 01-research-statement.md \
   -o 01-research-statement.pdf \
   -V "geometry:left=0.5in, right=0.5in, bottom=0.75in, top=0.5in" \
   --latex-engine=pdflatex \
+  --template="default.latex" \
   --filter=pandoc-fignos \
   --filter=pandoc-citeproc \
   --bibliography=papers-library.bib
